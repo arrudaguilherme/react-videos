034790c0a8507c3ed5329ea66746f21770f26859
Guilherme Arruda <guiga.arruda@hotmail.com>
2019-08-01 23:50:32 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar/index';
+
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )    
+    }
+}
+
+export default App;
